a74de6ae0f2776665280c7cddb8155ac87a8806b
zainafzalch <zainafzal6610b@gmail.com>
2024-07-06 22:16:21 -0400

    Added break for task 2 in question 3

--- question3.py
+++ question3.py
@@ -12,12 +12,13 @@
 
 # 83, 85, 86, 87, 88, 89, 90,
 
-# Task 2: Extract all the temperatures above 100. HINT: add the temperatures over 100 to a new list, or use list slicing to get the temperatures.
 
 temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
 print(temperatures[7:14])
 
 temp_over_100 = []
+
+# Task 2: Extract all the temperatures above 100. HINT: add the temperatures over 100 to a new list, or use list slicing to get the temperatures.
 
 for i in temperatures:
     if(i >= 100):
